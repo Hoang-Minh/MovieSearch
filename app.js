e0d3ebdcec89fc62dc0dccb3f752c6a7c790e589
Hoang-Minh <minhvuhoangnguyen@hotmail.com>
2018-02-19 01:46:03 -0800

    updated

--- app.js
+++ app.js
@@ -1,26 +1,18 @@
 $(document).ready(function() {
-    $(document).on("click", "#find-movie", function(event) {        
-      
-        if($("#movie-input").val() != "") {
+    $(document).on("click", "#find-movie", function(event) {
+        // Here we grab the text from the input box   
+        var movie = $("#movie-input").val();
+        if(movie != "") {
             event.preventDefault();
-            // Here we grab the text from the input box
-            var movie = $("#movie-input").val();
             // Here we construct our URL
             var queryURL = "https://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
-            // Write code between the dashes below to hit the queryURL with $ajax, then take the response data
-            // and display it in the div with an id of movie-view
-            // YOUR CODE GOES IN THESE DASHES. DO NOT MANUALLY EDIT THE HTML ABOVE
+            
             $.ajax({
                 url : queryURL,
                 method : 'GET'
               }
-            ).then(function(resp) {
-              
+            ).then(function(resp) {              
               console.log(resp);
-              
-              //var jsonStr = JSON.stringify(resp);
-              //$("#movie-view").append(jsonStr);
-              //var title = resp.Title;
               addMovie(resp);
               $("#movie-input").val(""); // clear the text in search box
             })
